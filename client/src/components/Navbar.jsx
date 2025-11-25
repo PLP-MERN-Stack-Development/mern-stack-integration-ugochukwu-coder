3b9a87642b9e06acf0083e0f6e80978b7b6e9885
Ugochukwu <gentleonlink@gmail.com>
2025-11-25 15:26:33 +0100

    updated tailwind

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 text-white p-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-xl font-bold">BlogApp</Link>
-        <div>
-          <Link to="/" className="mx-2 hover:underline">Home</Link>
-          <Link to="/create" className="mx-2 hover:underline">Create Post</Link>
+    <nav className="bg-blue-600 text-white shadow-md">
+      <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center p-4">
+        {/* Logo */}
+        <Link to="/" className="text-2xl font-bold mb-2 sm:mb-0">
+          BlogApp
+        </Link>
+
+        {/* Links */}
+        <div className="flex flex-col sm:flex-row gap-4">
+          <Link to="/" className="hover:underline transition">
+            Home
+          </Link>
+          <Link to="/create" className="hover:underline transition">
+            Create Post
+          </Link>
         </div>
       </div>
     </nav>
